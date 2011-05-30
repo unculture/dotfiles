76263b53a69d602fde6e80c97fc2d8e91d5d0857
Monkey <monkey@Lyra.(none)>
2011-05-30 13:13:16 +0100

    Added mysql  and php5 and php5-mysql, which I'm gonna need

--- gettingstarted.sh
+++ gettingstarted.sh
@@ -11,13 +11,13 @@ make install
 make install clean
 apt-get -y install libgnome2-dev libgtk2.0-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev
 make distclean
-~/Sources/vim/src/configure --with-features=huge --enable-gui=gnome2make
+~/Sources/vim/src/configure --with-features=huge --enable-gui=gnome2
 make install
 apt-get -y install ack-grep 
 ln -sf /usr/bin/ack-grep /usr/local/bin/ack
 apt-get -y install python-setuptools python-pip
 pip install virtualenv virtualenvwrapper
-export WORKON_HOME=~/Envs
+export WORKON_HOME=~/envs
 mkdir -p $WORKON_HOME
 source /usr/local/bin/virtualenvwrapper.sh
 apt-get -y install apache2
@@ -29,6 +29,11 @@ cd mod_wsgi-3.3
 ./configure
 make
 make install
+# have to do the below manaually for some reason :-(
+# echo "LoadModule wsgi_module /usr/lib/apache2/modules/mod_wsgi.so" > /etc/apache2/mods-available/wsgi.load
+# then a2enmod wsgi
+# this needs a password, or it'll block...apt-get -y install mysql-server
+apt-get -y install php5 php5-mysql
 cd ~
 git clone git://github.com/unculture/dotfiles.git vim
 mv vim/.vim .
@@ -44,5 +49,6 @@ winecfg
 cd ~/Sources
 wget http://www.kegel.com/wine/winetricks
 sh winetricks msxml6 gdiplus gecko vcrun2005
+# if the msxml isn't working, the 64 bit version I've got from M$ seems to work
 # now dig out the trial version of CS2 and install it... see http://wiki.winehq.org/AdobePhotoshop
 # To add ---
